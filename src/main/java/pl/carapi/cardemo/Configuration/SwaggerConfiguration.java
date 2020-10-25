248667f57de8e159c4362ab7a84ca78f7b798be5
Faron Norbert <faronnorbertkrk@gmail.com>
2020-10-25 20:57:29 +0100

    fix links problem

--- src/main/java/pl/carapi/cardemo/Configuration/SwaggerConfiguration.java
+++ src/main/java/pl/carapi/cardemo/Configuration/SwaggerConfiguration.java
@@ -1,18 +1,15 @@
-package pl.carapi.cardemo;
+package pl.carapi.cardemo.Configuration;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,15 +18,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@SpringBootApplication
-public class CarApiApplication {
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CarApiApplication.class, args);
+    @Bean
+    public Docket getDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("pl.carapi.cardemo"))
+                .build();
     }
 
-
-
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
     }
 
-
+}
